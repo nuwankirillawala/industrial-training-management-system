a2174e6f80fd2a1f7d40c1e586e96ba8024cb3e5
Nuwan Kirillawala <nuwan.sv.kirillawala@gmail.com>
2023-02-05 02:15:13 +0530

    update login for all users

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,20 +1,23 @@
-const Admin = require('../models/Admin');
 const jwt = require('jsonwebtoken');
+const Admin = require('../models/Admin');
+const Undergraduate = require('../models/Undergraduate');
+const Alumni = require('../models/Alumni');
+const Supervisor = require('../models/Supervisor');
 
 
 // handle errors
 const handleErrors = (err) => {
     console.log(err.message, err.code);
     let errors = { email: '', password: '', name: '', adminRole: '' };
-    
+
     //handle login errors
     //incorrect email
-    if(err.message === 'incorrect email'){
+    if (err.message === 'incorrect email') {
         errors.email = "That email is not regitered";
     }
-    
+
     //incorrect password
-    if(err.message === 'incorrect password'){
+    if (err.message === 'incorrect password') {
         errors.password = 'This password is incorrect';
     }
 
@@ -38,10 +41,35 @@ module.exports.login_post = async (req, res) => {
     console.log('login controller');
 
     try {
-        const admin = await Admin.login(email, password);
-        const token = createToken(admin._id);
+        let user, userID, userType;
+        try {
+            user = await Admin.login(email, password);
+            userID = user._id;
+            userType = "admin";
+        } catch (err) {
+            try {
+                user = await Undergraduate.login(email, password);
+                userID = user._id
+                userType = "undergraduate";
+            } catch (err) {
+                try {
+                    user = await Supervisor.login(email, password);
+                    userID = user._id
+                    userType = "supervisor";
+                } catch (err) {
+                    try {
+                        user = await Alumni.login(email, password);
+                        userID = user._id
+                        userType = "alumni";
+                    } catch (err) {
+                        throw new Error('incorrect email or password');
+                    }
+                }
+            }
+        }
+        const token = createToken(userID);
         res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-        res.status(200).json({ admin: admin._id });
+        res.status(200).json({ [userType]: userID });
     } catch (err) {
         const errors = handleErrors(err);
         res.status(400).json({ errors });
