a6c75b9910f2cb572dbb6e89b6310a9f20683c28
Gavesh Madushan <gaveshmadushan96@gmail.com>
2023-02-10 10:27:16 +0530

    add and create cmponent university logo

---
+++ client/src/components/Unilogo.jsx
@@ -0,0 +1,6 @@
+export const Unilogo = () => {
+    return (
+        <img src="../../public/images/unilogo.png" alt="university logo" width={'75px'} hieght={'150px'} />
+    )
+}
+// width={{md:'75px', sm:'50px', xs:'25px' }} hieght={{md:'150px', sm:'100px', xs:'50px' }}
