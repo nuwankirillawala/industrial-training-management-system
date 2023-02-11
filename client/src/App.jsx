072660c5df16bc3841343636dd64bd26f43a562d
hk-kusumi <91217989+hk-kusumi@users.noreply.github.com>
2023-02-11 16:57:48 +0530

    Page Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Login } from './components/user/Login'
+import Navbar from './components/Navbar';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +13,7 @@ function App() {
     <Router>
       <Routes>
         <Route exact path='/' element={<Login/>}/>
-        
+        <Route exact path='/navbar' element={<Navbar/>}/> 
       </Routes>
     </Router>
   )
