5f0d39229e8ed0449fcd79eaad1e84b20437bb3f
Nuwan Kirillawala <nuwan.sv.kirillawala@gmail.com>
2023-02-15 10:33:05 +0530

    move

---
+++ client/src/components/Login/Login.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import {Stack,Typography,Box,TextField,Button} from '@mui/material';
+import {Unilogo} from '../shared/Images/Unilogo'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+
+export const Login = () => {
+    // we have update error handling, responsiveness.
+
+    const[email,setEmail]=useState('');
+    const[password,setPassword]=useState('');
+    const navigate=useNavigate();
+
+    const onSubmit = async(e) => {
+        e.preventDefault()
+        console.log({email,password});
+
+
+        try{
+            const data = {email,password}
+            const res = await axios.post('http://localhost:5173/login',data)
+            console.log({res})
+
+            if(res.status===200){
+                console.log('success');
+                // in here we have to change navigate location acording to the user. currently no navigaton.
+                navigate('/adminDash')
+            }
+        }
+
+        catch(err){
+            console.log({err})
+        }
+
+    }
+    return(
+        <Stack direction={'column'} backgroundColor={'#EAF0FF'} height={'100vh'} spacing={3} justifyContent={'space-evenly'}>
+            <Stack direction={'row'} alignItems={'center'} justifyContent={'center'} spacing={4}>
+                <Stack>
+                    <Unilogo width={'75px'} height={'150px'}/>
+                </Stack>
+                <Stack alignItems={'center'}>
+                    <Typography variant={'h3'} color={'#7F3A41'} fontWeight={'bold'} fontStyle={'inter'}>UNIVERSITY OF RUHUNA</Typography>
+                    <Typography variant={'h6'} color={'#4665D1'} fontWeight={'bold'} fontStyle={'inter'}>INDUSTRIAL TRAINING MANAGEMENT SYSTEM</Typography>
+                </Stack>
+            </Stack>
+
+            {/* login form */}
+            <Stack alignItems={'center'} justifyContent={'center'}>
+                <Box backgroundColor={'#FFFF'} padding={'30px'} borderRadius={'30px'} boxShadow={5}>
+                    <Stack direction={'column'} alignItems={'center'} spacing={3}>
+                        <Stack>
+                            <Typography variant={'h6'} fontWeight={'bold'} fontStyle={'inter'} color={'#4665D1'}>LOGIN</Typography>
+                        </Stack>
+                        <Stack>
+                            <form onSubmit={onSubmit}>
+                                <Stack direction={'column'} spacing={3} alignItems={'center'}>
+                                    <TextField
+                                        id="outlined-basic"
+                                        label="Username"
+                                        variant="outlined"
+                                        type="text"
+                                        placeholder="Enter Your Username"
+                                        onChange={e=>setEmail(e.target.value)}
+                                        required
+                                    />
+                                    <TextField
+                                        id="outlined-basic"
+                                        label="Password"
+                                        variant="outlined"
+                                        type="password"
+                                        placeholder="Enter Your Password"
+                                        onChange={e=>setPassword(e.target.value)}
+                                        required
+                                    />
+                                    <a href='http://google.com'>
+                                    <Typography variant={'caption'} fontWeight={'bold'} color={'#4665D1'}>Fogot Password?</Typography>
+                                    </a>
+                                    <Button type="submit" variant="contained" sx={{borderRadius:'15px' , width:'100%'}}>LOGIN</Button>
+                                </Stack>
+                            </form>
+                        </Stack>
+                    </Stack>
+                </Box>
+            </Stack>
+            
+        </Stack>
+        
+    );
+}
---
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,118 @@
+import * as React from 'react';
+import { Box , ListItemButton, Stack } from '@mui/material';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import ListItem from '@mui/material/ListItem';
+// import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import { Unilogo } from '../shared/Images/Unilogo';
+import { Grid } from '@mui/material';
+import { Padding } from '@mui/icons-material';
+import { styled } from '@mui/system';
+import { alignProperty } from '@mui/material/styles/cssUtils';
+
+const drawerWidth = 240;
+// const listItemButtonWidth = 145;
+
+// const listItemButtonWidth = styled(ListItemButton)({
+//   width: 150,
+// });
+
+export default function PermanentDrawerLeft() {
+
+  const menuItems = ['Dashboard', 'CV', 'Daily Report', 'Company', 'Notice'];
+
+   return (
+    <Box sx={{ display: 'flex' , backgroundColor:'#757ce8'}}>
+      <CssBaseline />
+      
+      <Drawer
+        sx={{
+          textAlign: 'center',        
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+            bgcolor: '#4665d2',
+            borderRadius: '0px 20px 20px 0px',
+            color: '#f4f6fc',            
+          },
+          // alignContent:'center',
+          // alignItems:'center',
+          // justifyContent:'center',
+          // justifyItems:'center'
+        }}
+        
+        variant="permanent"
+        anchor="left">
+        
+        <Grid container /*direction={'row'}*/ justifyContent="center">
+          <Stack sx={{ position:'relative', top:20 }}>
+            <Unilogo width='50px' height='100px'/>
+          </Stack>
+        </Grid>
+        
+
+        <Stack>
+          <Typography
+            variant={'h6'}
+            fontWeight={'bold'}
+            sx={{
+              position:'relative',
+              top:30,
+              lineHeight:1.2
+            }}>
+            ITMS
+          </Typography>
+        </Stack>
+        
+        <Stack sx={{position:'relative', top:150, alignItems:'center' ,justifyContent:'center'}} style={{ alignContent:'center', justifyContent:'center'}}>
+          {menuItems.map((text, index) => (
+            <ListItem key={text} disablePadding >
+            <Grid /*container*/ direction={'column'} justifyContent="center"  />
+              <Box sx={{ width:10, alignItems:'center' ,justifyContent:'center' }}/>
+                <ListItemButton
+                  sx={{
+                    height: 35,
+                    // width: 10,
+                    textAlign:'center',                  
+                    '&:hover': {
+                        bgcolor: '#f4f6fc',
+                        color: 'black'
+                      },
+                    borderRadius:'10px 10px 10px 10px',
+                    // Padding:'0px 10px 0px 10px'
+                    // width:10                    
+                  }}>                  
+                  <ListItemText primary={text} />
+                </ListItemButton>
+            </ListItem>
+          ))}
+        </Stack>       
+        
+        <Stack sx={{position:'relative', top:250}}>
+          {['Settings', 'Back', 'Logout'].map((text, index) => (
+            <ListItem key={text} disablePadding>                
+                <ListItemButton
+                  sx={{
+                    height: 35,
+                    textAlign:'center',                    
+                    '&:hover': {
+                      bgcolor: '#f4f6fc',
+                      color: 'black'
+                    },
+                    borderRadius:'10px 10px 10px 10px'
+                  }}>                 
+                  <ListItemText primary={text} />
+                </ListItemButton>                
+              </ListItem>
+            ))}        
+        </Stack>        
+
+      </Drawer>
+      
+    </Box>
+  );
+}
