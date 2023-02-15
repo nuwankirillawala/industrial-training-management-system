d27c498c24f2d906606ee51579cb16e57f390afe
SSumu <sadeepalsumudu@gmail.com>
2023-02-15 08:24:57 +0530

    Add initial sidebar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,8 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Login } from './components/user/Login'
-import Navbar from './components/Navbar';
-
+import Navbar from './components/card/basicCard/Navbar'
+import Sidebar from './components/card/basicCard/Sidebar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +14,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Login/>}/>
         <Route exact path='/navbar' element={<Navbar/>}/> 
+        <Route exact path='/sidebar' element={<Sidebar/>}/> 
       </Routes>
     </Router>
   )
