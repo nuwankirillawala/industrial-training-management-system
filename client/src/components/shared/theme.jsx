e95ceb4017238d463cc8a9012cfb5813343090a1
Gavesh Madushan <gaveshmadushan96@gmail.com>
2023-02-21 16:52:31 +0530

    update theme

--- client/src/components/shared/theme.jsx
+++ client/src/components/shared/theme.jsx
@@ -5,82 +5,75 @@ const baseFontColor = "#363853"
 
 const theme = createTheme ({
 
-    // background: {
-    //     default: "#F5F8FF",
-    //     paper: "#F5F8FF",
-    // },
+    background: {
+        default: "#F5F8FF",
+        paper: "#F5F8FF",
+    },
 
-    // palette: {
-    //     common: {
-    //         black: baseFontColor
-    //     },
+    palette: {
+        common: {
+            black: baseFontColor
+        },
 
-    //     primary: {
-    //         main: baseColor,
-    //     },
-    //     text: {
-    //         primary: baseFontColor,
-    //         secondary: '#ffff'
-    //     }
+        primary: {
+            main: baseColor,
+        },
+        text: {
+            primary: baseFontColor,
+        }
 
-    // },
-    // shape: {
-    //     // borderRadius: 100,
-    // },
+    },
 
-    // typography:{
-    //     body2: {
-    //         color: baseFontColor
-    //     },
-    //     body1: {
-    //         color: '#EFF4FF'
-    //     }
-    // },
+    typography:{
+        body2: {
+            color: baseFontColor
+        },
+    },
     
-    // components: {
-    //     MuiButton: {
-    //         styleOverrides: {
-    //             contained: {
-    //                 color: baseColor,
-    //             },
-    //         },
-    //         defaltProps: {
-    //             disableRipple: true
-    //         },
-    //     },
-    //     MuiPopover: {
-    //         styleOverrides: {
-    //             paper: {
-    //                 // borderRadius: '20px'
-    //             }
-    //         }
-    //     },
-    //     MuiDrawer: {
-    //         styleOverrides: {
-    //             paper: {
-    //                 backgroundColor: baseColor,
-    //                 borderTopRightRadius: '15px',
-    //                 borderBottomRightRadius: '10px',
-    //             }
-    //         }
-    //     },
-    //     MuiAppBar: {
-    //         styleOverrides: {
-    //             colorPrimary: {
-    //                 backgroundColor: "#EFF4FF"
-    //             },
-    //             root: {
-    //                 borderRadius: '0px 0px 0px 10px'
-    //             }
+    components: {
+        MuiButton: {
+            // styleOverrides: {
+            //     contained: {
+            //         color: baseColor,
+            //     },
+            // },
+            defaltProps: {
+                disableRipple: true
+            },
+        },
+        MuiPopover: {
+            styleOverrides: {
+                paper: {
+                    // borderRadius: '20px'
+                }
+            }
+        },
+        MuiDrawer: {
+            styleOverrides: {
+                paper: {
+                    backgroundColor: baseColor,
+                    borderTopRightRadius: '15px',
+                    borderBottomRightRadius: '10px',
+                }
+            }
+        },
+        MuiAppBar: {
+            styleOverrides: {
+                colorPrimary: {
+                    backgroundColor: "#EFF4FF"
+                },
+                root: {
+                    borderRadius: '0px 0px 0px 10px'
+                }
                 
-    //         }
-    //     },
-    //     MuiIconButton: {
-    //         styleOverrides: {
-    //             colorPrimary: '#EAFOFF'
-    //         }
-    //     },
-    // },
+            }
+        },
+        MuiIconButton: {
+            styleOverrides: {
+                colorPrimary: '#EAFOFF'
+            }
+        },
+    },
 });
 
 export default theme;
