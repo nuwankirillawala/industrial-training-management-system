cf25f07e02bd37e7af88fb5ec74b5ede61ae61b1
NoV4k <sanjanadiluka@gmail.com>
2023-02-26 12:30:45 +0530

    cvupdate

--- client/src/components/card/basicCard/BasicCard.jsx
+++ client/src/components/card/basicCard/BasicCard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Box } from '@mui/material'
 import Navbar from '../../Navbar/Navbar'
 import Sidebar from '../../Sidebar/Sidebar'
+import { CvUpdate } from '../../user/Undergraduate/Pages/CvUpdate'
 
 export const BasicCard = ({children}) => {
   return (
@@ -11,6 +12,7 @@ export const BasicCard = ({children}) => {
       </Box>
       <Box sx={{display: 'flex', gap: '10px', flexDirection: 'column', flex: 'auto'}}>
         <Navbar/>
+        <CvUpdate/>
       </Box>
     </Box>    
   )
