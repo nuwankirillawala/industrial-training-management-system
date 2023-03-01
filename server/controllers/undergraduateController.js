8e1c9fd33d936f5f08c90ea70146a87757f6d57b
Nuwan Kirillawala <nuwan.sv.kirillawala@gmail.com>
2023-03-01 11:27:24 +0530

    add comments

--- server/controllers/undergraduateController.js
+++ server/controllers/undergraduateController.js
@@ -78,13 +78,17 @@ module.exports.companySelection = async (req, res) => {
 module.exports.undergraduateDashboard = async (req, res) => {
     try {
         const userId = req.body.id // 🛑 user id must get from jwt in future 🛑
+        
         // get all data exept password
         const user = await Undergraduate.findById(userId).select('-password');
+
         if(!user){
             res.status(404).json({message: "user not found!"});
         }
 
+        //get all companies
         const companies = await Company.find();
+
         res.status(200).json({user, companies});
     } catch (err) {
         res.status(500).json({message: "server error!"});
