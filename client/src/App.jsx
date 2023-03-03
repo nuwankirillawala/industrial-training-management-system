ebe6246e4a467c8951c6dc87d2362ff01a10549f
NoV4k <sanjanadiluka@gmail.com>
2023-03-03 10:59:58 +0530

    routes fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,76 +37,76 @@ import { StudentCompnay } from './Pages/Undergraduate/StudentCompnay'
 
 function App() {
 
-  // const router = createBrowserRouter(createRoutesFromElements(
-  //   <Route path='/' element= { <RootLayout/> }>
-  //     <Route path='/' element= { <Layout/> }>
-  //       <Route path='cvupdate' element= { <CvUpdate/> }></Route>
-  //       <Route path='manageuser' element={ <Manageuser/> }></Route>
-  //       <Route path='createuser' element={ <CreateUser/> }></Route>
-  //       <Route path='stddash' element={<StdDashboard/>}></Route>
-  //     </Route>
-  //     <Route path='/'>
-  //       <Route path='login' element={ <Login/> }></Route>
-  //     </Route>
-  //     <Route path='/'>
-  //       <Route path='test' element={ <CvUpdate/> }></Route>
-  //     </Route>
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path='/' element= { <RootLayout/> }>
+      <Route path='/' element= { <Layout/> }>
+        <Route path='cvupdate' element= { <CvUpdate/> }></Route>
+        <Route path='manageuser' element={ <Manageuser/> }></Route>
+        <Route path='createuser' element={ <CreateUser/> }></Route>
+        <Route path='stddash' element={<StdDashboard/>}></Route>
+      </Route>
+      <Route path='/'>
+        <Route path='login' element={ <Login/> }></Route>
+      </Route>
+      <Route path='/'>
+        <Route path='test' element={ <CvUpdate/> }></Route>
+      </Route>
 
-  //   </Route>
-  // ))
+    </Route>
+  ))
 
-  //   return (
-  //     <>
-  //       <RouterProvider router={router}/>
-  //     </>
-  //   )
-  return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Routes>
-          <Route exact path='/' element={<Login />} />
-          <Route exact path='/navbar' element={<Navbar />} />
-          <Route exact path='/sidebar' element={<Sidebar />} />
-          <Route exact path='/create-user' element={<CreateUser />} />
-          <Route exact path='/basiccard' element={<BasicCard />} />
-          <Route exact path='/user-profile' element={<UserProfile />} />
-          <Route exact path='/stddash' element={<StdDashboard />} />
-          {/* <Route exact path='/stdcompany' element={<StdCompnay />} /> */}
-          <Route exact path='/layout' element={<Layout />} />
-          <Route exact path='/cvupdate' element={<CvUpdate />} />
-          <Route exact path='/manageuser' element={<Manageuser />} />
-          <Route exact path='/addadmin' element={<AddAdmin />} />
-          <Route exact path='/view-admin-details' element={<ViewAdmin />} />
-          <Route exact path='/add-superv-details' element={<AddSuperv />} />
-          <Route exact path='/view-superv-details' element={<ViewSuperv />} />
-          <Route exact path='/add-undg-details' element={<AddUndg />} />
-          <Route exact path='/view-undg-details' element={<ViewUndg />} />
-          <Route exact path='/add-comp-details' element={<AddCompany />} />
-          <Route exact path='/view-comp-details' element={<ViewCompany />} />
-          <Route exact path='/add-alumini-details' element={<AddAlumini />} />
-          <Route exact path='/view-alumini-details' element={<ViewAlumini />} />
-          <Route exact path='/dialogbox' element={<Dialogbox title="Update Administrator"><Admin /></Dialogbox>} />
-          <Route exact path='/' element={<Login/>}/>
-          <Route exact path='/navbar' element={<Navbar/>}/> 
-          <Route exact path='/sidebar' element={<Sidebar/>}/>
-          <Route exact path='/create-user' element={<CreateUser/>}/> 
-          <Route exact path='/basiccard' element={<BasicCard/>}/> 
-          <Route exact path='/user-profile' element={<UserProfile />} />
-          <Route exact path='/stddash' element={<StudentDashboard />} />
-          <Route exact path='/stdcompany' element={<StudentCompnay />} />
-          <Route exact path='/layout' element={<Layout />} />
+    return (
+      <>
+        <RouterProvider router={router}/>
+      </>
+    )
+//   return (
+//     <ThemeProvider theme={theme}>
+//       <Router>
+//         <Routes>
+//           <Route exact path='/' element={<Login />} />
+//           <Route exact path='/navbar' element={<Navbar />} />
+//           <Route exact path='/sidebar' element={<Sidebar />} />
+//           <Route exact path='/create-user' element={<CreateUser />} />
+//           <Route exact path='/basiccard' element={<BasicCard />} />
+//           <Route exact path='/user-profile' element={<UserProfile />} />
+//           <Route exact path='/stddash' element={<StdDashboard />} />
+//           {/* <Route exact path='/stdcompany' element={<StdCompnay />} /> */}
+//           <Route exact path='/layout' element={<Layout />} />
+//           <Route exact path='/cvupdate' element={<CvUpdate />} />
+//           <Route exact path='/manageuser' element={<Manageuser />} />
+//           <Route exact path='/addadmin' element={<AddAdmin />} />
+//           <Route exact path='/view-admin-details' element={<ViewAdmin />} />
+//           <Route exact path='/add-superv-details' element={<AddSuperv />} />
+//           <Route exact path='/view-superv-details' element={<ViewSuperv />} />
+//           <Route exact path='/add-undg-details' element={<AddUndg />} />
+//           <Route exact path='/view-undg-details' element={<ViewUndg />} />
+//           <Route exact path='/add-comp-details' element={<AddCompany />} />
+//           <Route exact path='/view-comp-details' element={<ViewCompany />} />
+//           <Route exact path='/add-alumini-details' element={<AddAlumini />} />
+//           <Route exact path='/view-alumini-details' element={<ViewAlumini />} />
+//           <Route exact path='/dialogbox' element={<Dialogbox title="Update Administrator"><Admin /></Dialogbox>} />
+//           <Route exact path='/' element={<Login/>}/>
+//           <Route exact path='/navbar' element={<Navbar/>}/> 
+//           <Route exact path='/sidebar' element={<Sidebar/>}/>
+//           <Route exact path='/create-user' element={<CreateUser/>}/> 
+//           <Route exact path='/basiccard' element={<BasicCard/>}/> 
+//           <Route exact path='/user-profile' element={<UserProfile />} />
+//           <Route exact path='/stddash' element={<StudentDashboard />} />
+//           <Route exact path='/stdcompany' element={<StudentCompnay />} />
+//           <Route exact path='/layout' element={<Layout />} />
 
-          <Route exact path='/adminform' element={<Admin />} />
-          <Route exact path='/aluminiform' element={<Alumini />} />
-          <Route exact path='/companyform' element={<Company />} />
-          <Route exact path='/supervisorform' element={<Supervisor />} />
-          <Route exact path='/undergraduateform' element={<Undergraduate />} />
-        <Route exact path='/notice' element={<Notice />} />
+//           <Route exact path='/adminform' element={<Admin />} />
+//           <Route exact path='/aluminiform' element={<Alumini />} />
+//           <Route exact path='/companyform' element={<Company />} />
+//           <Route exact path='/supervisorform' element={<Supervisor />} />
+//           <Route exact path='/undergraduateform' element={<Undergraduate />} />
+//         <Route exact path='/notice' element={<Notice />} />
 
-        </Routes>
-      </Router>
-    </ThemeProvider>
-  )
-}
+//         </Routes>
+//       </Router>
+//     </ThemeProvider>
+//   )
+// }
 
 export default App
