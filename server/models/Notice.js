5e1e6f87941c8cef3a61099a1dba939dcb2f8dff
Nuwan Kirillawala <nuwan.sv.kirillawala@gmail.com>
2023-03-05 18:15:02 +0530

    update schema with editLog

--- server/models/Notice.js
+++ server/models/Notice.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Admin = require('../models/Admin');
 
 const noticeSchema = new mongoose.Schema({
-    title:{
+    title: {
         type: String,
         required: [true, "notice title is required"]
     },
@@ -10,15 +10,44 @@ const noticeSchema = new mongoose.Schema({
         type: String,
         required: [true, "notice body is required"]
     },
-    author:{
+    author: {
         type: mongoose.Schema.Types.ObjectId,
         ref: Admin,
         required: [true, "notice author is required"]
     },
-    publishedDate:{
+    publishedDate: {
         type: Date,
-        required: [true, "published date is required"]
-    }
+        default: Date.now
+    },
+    editLog: [{
+        status: {
+            type: String,
+            enum:['created', 'edited'],
+            required: true
+        },
+        title: {
+            type: String,
+            required: [true, "notice title is required"]
+        },
+        body: {
+            type: String,
+            required: [true, "notice body is required"]
+        },
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: Admin,
+            required: [true, "notice author is required"]
+        },
+        editDate: {
+            type: Date,
+            required: true
+        }
+    }]
+});
+
+noticeSchema.post('save', function (doc, next) {
+    console.log('new notice is created', doc);
+    next();
 })
 
 const Notice = new mongoose.model('notice', noticeSchema);
