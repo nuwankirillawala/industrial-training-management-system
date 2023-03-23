d42c4a04f66e5b2fe329b1f31adb93f12f0fda65
Gavesh Madushan <gaveshmadushan96@gmail.com>
2023-03-24 02:08:46 +0530

    remove extra grid student-comapny

--- client/src/Pages/Undergraduate/StudentCompany.jsx
+++ client/src/Pages/Undergraduate/StudentCompany.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Tile } from '../../components/card/Tile'
-import { Grid, Box, Typography} from '@mui/material'
+import { Grid, Box, Typography, Stack} from '@mui/material'
 import { Table, TableBody, TableCell, TableHead, TableRow, TableContainer } from '@mui/material'
 
 import { StudentAddCompany } from '../../components/user/Undergraduate/StudentAddCompany'
@@ -15,90 +15,86 @@ export const StudentCompany = () => {
     const[page,setPage] = useState({no: 1})
 
   return (
-            <Grid container spacing={1}>
-                <Grid item md={7}>
+    <Box overflow={'scroll'} maxHeight='89vh'>
+        <Grid container spacing={1}>
+            
+            <Grid item md={7}>
 
-                    <Grid container spacing={1}>
+                <Stack spacing={1}>
 
 {/* company selection  */}
-                        <Grid item md={12}>
-                            <StudentCompanyChoice />
-                        </Grid>
+                    <Stack>
+                        <StudentCompanyChoice />
+                    </Stack>
 
 {/* company status, add company, inter time period component add dinamikally */}
-                        <Grid item md={12}>
-                            <Formik>
-                            {({})=>(
-                                <Box>
-                                    {page.no === 1 && (
-                                        <StudentCompanyStatus pageNo={page} setPage={setPage}/>
-                                    
-                                    )}
-                                    {page.no === 2 && (
-                                        <StudentAddCompany pageNo={page} setPage={setPage}/>
-                                    )}
-                                    {page.no === 3 && (
-                                        <StudentInternPeriod pageNo={page} setPage={setPage}/>
-                                    )}
-                                </Box>
-                            )}
-                            </Formik>
-                        </Grid>
-                        
-                    </Grid>
-                </Grid>
-
-
-                <Grid item md={5}>
-                    <Tile height={'90vh'}>
-                        <Grid container>
-                            <Grid item md={12}>
-                                        
-                                    <Typography variant='h6' fontWeight={'bold'}>Company Ranking List</Typography>
-                                        
-                                    <TableContainer>
-                                        <Table size='small'>
-                                            <TableHead>
-                                                <TableRow>
-                                                    <TableCell align='left'>Company</TableCell>
-                                                    <TableCell align='right'>Job Role</TableCell>
-                                                    <TableCell align='right'>Rating</TableCell>
-                                                    <TableCell align='right'># UOR Graduates</TableCell>
-                                                </TableRow>
-                                            </TableHead>
-                                            <TableBody>
-                                                <TableRow>
-                                                    <TableCell align='left'>WSO2</TableCell>
-                                                    <TableCell align='right'>SE</TableCell>
-                                                    <TableCell align='right'>8.5</TableCell>
-                                                    <TableCell align='right'>10</TableCell>
-                                                </TableRow>
-                                                
-                                                <TableRow>
-                                                    <TableCell align='left'>WSO2</TableCell>
-                                                    <TableCell align='right'>SE</TableCell>
-                                                    <TableCell align='right'>8.5</TableCell>
-                                                    <TableCell align='right'>10</TableCell>
-                                                </TableRow>
-                                                
-                                                <TableRow>
-                                                    <TableCell align='left'>WSO2</TableCell>
-                                                    <TableCell align='right'>SE</TableCell>
-                                                    <TableCell align='right'>8.5</TableCell>
-                                                    <TableCell align='right'>10</TableCell>
-                                                </TableRow>
-                                                
-                                            </TableBody>
-                                        </Table>
-                                    </TableContainer>
-                                        
-                                        
-                            </Grid>
-                        </Grid>
-                    </Tile>
-                </Grid>
-
+                    <Stack>
+                        <Formik>
+                        {({})=>(
+                            <Box>
+                                {page.no === 1 && (
+                                    <StudentCompanyStatus pageNo={page} setPage={setPage}/>
+                                
+                                )}
+                                {page.no === 2 && (
+                                    <StudentAddCompany pageNo={page} setPage={setPage}/>
+                                )}
+                                {page.no === 3 && (
+                                    <StudentInternPeriod pageNo={page} setPage={setPage}/>
+                                )}
+                            </Box>
+                        )}
+                        </Formik>
+                    </Stack>
+                </Stack>
             </Grid>
+
+            <Grid item md={5}>
+                <Tile height={'90vh'}>
+                    <Stack>
+                                    
+                        <Typography variant='h6' fontWeight={'bold'}>Company Ranking List</Typography>
+                            
+                        <TableContainer>
+                            <Table size='small'>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell align='left'>Company</TableCell>
+                                        <TableCell align='right'>Job Role</TableCell>
+                                        <TableCell align='right'>Rating</TableCell>
+                                        <TableCell align='right'># UOR Graduates</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell align='left'>WSO2</TableCell>
+                                        <TableCell align='right'>SE</TableCell>
+                                        <TableCell align='right'>8.5</TableCell>
+                                        <TableCell align='right'>10</TableCell>
+                                    </TableRow>
+                                    
+                                    <TableRow>
+                                        <TableCell align='left'>WSO2</TableCell>
+                                        <TableCell align='right'>SE</TableCell>
+                                        <TableCell align='right'>8.5</TableCell>
+                                        <TableCell align='right'>10</TableCell>
+                                    </TableRow>
+                                    
+                                    <TableRow>
+                                        <TableCell align='left'>WSO2</TableCell>
+                                        <TableCell align='right'>SE</TableCell>
+                                        <TableCell align='right'>8.5</TableCell>
+                                        <TableCell align='right'>10</TableCell>
+                                    </TableRow>
+                                    
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Stack>
+                </Tile>
+            </Grid>
+        </Grid>
+    </Box>
     
   )
 }
