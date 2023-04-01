ece26c3c25031e326fcad329cc3149b73e2f1648
Gavesh Madushan <gaveshmadushan96@gmail.com>
2023-04-02 00:51:13 +0530

    comment "{console.log(sx)}"

--- client/src/components/card/Tile.jsx
+++ client/src/components/card/Tile.jsx
@@ -5,7 +5,7 @@ export const Tile = ({width,height,children,sx}) => {
   return (
     <Box backgroundColor={'#EAF0FF'} width={width} height={height} borderRadius={2} sx={{overflow: 'auto'}} padding={'10px 20px 10px 20px'}>
       {children}
-      {console.log(sx)}
+      {/* {console.log(sx)} */}
     </Box>
   )
 }
