57732b9b57cbb7bb752c8ee8c9830b7502b8e2f2
hk-kusumi <91217989+hk-kusumi@users.noreply.github.com>
2023-04-23 00:39:17 +0530

    Update view users

--- client/src/components/user/Admin/viewusers/ViewAlumini.jsx
+++ client/src/components/user/Admin/viewusers/ViewAlumini.jsx
@@ -1,12 +1,13 @@
 import { Stack } from "@mui/system";
 import React from "react";
 import { useState, useEffect } from "react";
-import { Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import { Table, TableBody, TableCell, TableHead, TableRow, Typography, Grid } from "@mui/material";
 import { Tile } from "../../../card/Tile";
 
 export const ViewAlumini = () => {
     const [Column, setColumn] = useState([])
     const [Records, setRecords] = useState([])
+    const [singleUser, setSingleUser] = useState([])
 
     useEffect(() => {
         fetch('https://dummyjson.com/products')  //url need to changed into json url(this is dummy data from a site)
@@ -18,32 +19,52 @@ export const ViewAlumini = () => {
     }, [])
 
     return (
-        <Tile>
-            <Stack>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            {Column.map((c, i) =>
-                                <TableCell key={i}>
-                                    {c}
-                                </TableCell>
-                            )}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
+        <Grid spacing={1} container>
+            <Grid item md={12} sm={12} ><Typography variant='subtitle1'> View Alumni Details</Typography> </Grid>
+            <Grid item md={8} sm={8} >
+                <Tile>
+                    <Stack>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    {Column.map((c, i) =>
+                                        <TableCell key={i}>
+                                            {c}
+                                        </TableCell>
+                                    )}
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
 
-                        {Records.map((r, i) =>
-                            <TableRow key={i}>
-                                <TableCell >   {r.id}  </TableCell>
-                                <TableCell >   {r.title}  </TableCell>
-                                <TableCell >   {r.description} </TableCell>
-                            </TableRow> //id,title,description need to change as json file
-                        )}
+                                {Records.map((r, i) =>
+                                    <TableRow key={i} onClick={() => setSingleUser(r)}>
+                                        <TableCell >   {r.id}  </TableCell>
+                                        <TableCell >   {r.title}  </TableCell>
+                                        <TableCell >   {r.description} </TableCell>
+                                    </TableRow> //id,title,description need to change as json file
+                                )}
 
-                    </TableBody>
-                </Table>
+                            </TableBody>
+                        </Table>
 
-            </Stack>
-        </Tile>
+                    </Stack>
+                </Tile>
+            </Grid>
+
+            <Grid item md={4} sm={4} > <Tile>
+                <Stack direction={'column'}>
+                    <Typography fontWeight={'bold'} paddingTop={'15px'} paddingBottom={'15px'}>Alumni full details</Typography>
+                    {singleUser && (
+                        <Stack direction={'column'}>
+                            <Stack direction={'row'}>  <Typography width={'135px'}> Name </Typography><Typography> {singleUser.id} </Typography></Stack>
+                            <Stack direction={'row'}> <Typography width={'135px'}> Staff ID </Typography><Typography>{singleUser.title} </Typography></Stack>
+                            <Stack direction={'row'}> <Typography width={'135px'}> E-mail</Typography><Typography> {singleUser.title} </Typography></Stack>
+                            <Stack direction={'row'}> <Typography width={'135px'}> Contact Number</Typography><Typography>{singleUser.title} </Typography></Stack>
+                        </Stack>
+                    )}
+                </Stack>
+            </Tile></Grid>
+
+        </Grid>
     )
 } 
