e2304147b8828cab4f96c0e8e708451db3c24171
NoV4k <sanjanadiluka@gmail.com>
2023-04-27 02:57:31 +0530

    supervisor selection

--- client/src/Pages/Admin/Supervisor/AssignSupervisorForIntern.jsx
+++ client/src/Pages/Admin/Supervisor/AssignSupervisorForIntern.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { Box, Stack, Typography } from "@mui/material";
+import React from "react";
+import { Tile } from "../../../components/card/Tile";
 
 const AssignSupervisorForIntern = () => {
-  return (
-    <div>AssignSupervisorForIntern</div>
-  )
-}
+  //column for data grid
 
-export default AssignSupervisorForIntern
+  //End of column for data grid
+
+  return (
+    <Box sx={{ height: "88vh" }}>
+      <Box sx={{ width: "100%" }}>
+        <Typography
+          variant="h6"
+          color="primary"
+          marginBottom={"5px"}
+          paddingLeft={"15px"}
+        >
+          Assign Supervisors For Interns
+        </Typography>
+      </Box>
+      <Box sx={{ height: "100%" }}>
+        <Stack spacing={1} height={"100%"}>
+          <Tile sx={{ height: "100%" }}>
+            <Box sx={{ height: 400, width: "80vw" }}>hello</Box>
+          </Tile>
+        </Stack>
+      </Box>
+    </Box>
+  );
+};
+
+export default AssignSupervisorForIntern;
