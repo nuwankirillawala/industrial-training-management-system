e68d8509d5a8ff8a9e29f750c3c2aa8088939175
hk-kusumi <91217989+hk-kusumi@users.noreply.github.com>
2023-05-10 10:40:14 +0530

    alumni update backend

--- server/controllers/alumniController.js
+++ server/controllers/alumniController.js
@@ -34,25 +34,22 @@ module.exports.updateAlumniProfile = catchAsync(async (req, res) => {
     try {
         const userId = req.body.id;
         const { name, email, contactNo, regNo, graduatedYear } = req.body;
-
-        const filter = { _id: userId };
         const update = { $set: { name, email, contactNo, regNo, graduatedYear } };
         const options = { new: true };
 
         // findbyIdAndUpdate mongoose⚡
 
-        await Admin.updateOne(filter, update, options)
-            .then(async () => {
-                const user = await Alumni.findOne(filter);
-                if (!user) {
-                    return res.status(400).json({ message: "user not exists" });
-                }
-                res.status(200).json(user);
-            })
-            .catch((error) => {
-                console.log(error.message);
-                res.status(400).json(error);
-            });
+        const user = await Alumni.findByIdAndUpdate(
+            userId,
+            update,
+            options
+        );
+        
+        if (!user) {
+            return res.status(400).json({ error: "user not found" });
+        }
+
+        res.status(200).json(user);
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
