bf3d78e5133a0d3197a45bc515f9a46f63e6cadb
Gavesh Madushan <gaveshmadushan96@gmail.com>
2023-05-24 02:22:59 +0530

    seperating cvupload

--- client/src/Pages/Undergraduate/StudentCvUpdate.jsx
+++ client/src/Pages/Undergraduate/StudentCvUpdate.jsx
@@ -1,35 +1,13 @@
 import React from "react";
 import { useState } from "react";
-import {
-  Box,
-  Grid,
-  Typography,
-  Button,
-  Stack,
-  Dialog,
-  Slide,
-  IconButton,
-  Toolbar,
-  AppBar,
-  Chip,
-} from "@mui/material";
+import { Box, Grid, Typography, Stack, IconButton, Chip } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
-import CloseIcon from "@mui/icons-material/Close";
 import { Tile } from "../../components/card/Tile";
-import { Viewer, Worker } from "@react-pdf-viewer/core";
-import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import { EnglishProficiency } from "../../components/user/Undergraduate/studentCV/EnglishProficiency";
 import { PopUpDialog } from "../../components/user/Undergraduate/studentCV/PopUpDialog";
-import { StatusSnackBar } from "../../components/StatusSnackBar/StatusSnackBar";
-import axios from "axios";
-
-//creating transition for dialog
-const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
-//end of creation transition for dialog
+import { CVUpload } from "../../components/user/Undergraduate/studentCV/CVUpload";
 
 export const StudentCvUpdate = () => {
   //Get data from back end
@@ -52,18 +30,18 @@ export const StudentCvUpdate = () => {
   //End of fetcching data from the back end
 
   //Data for End point
-  const [englishProficiency, setEnglishProficiency] = useState({
-    olResult: "",
-    alResult: "",
-    speakingLevel: "",
-    writingLevel: "",
-    readingLevel: "",
-  });
+  // const [englishProficiency, setEnglishProficiency] = useState({
+  //   olResult: "",
+  //   alResult: "",
+  //   speakingLevel: "",
+  //   writingLevel: "",
+  //   readingLevel: "",
+  // });
 
-  const passDataFromChild = (newValue) => {
-    setEnglishProficiency(newValue);
-    // console.log(englishProficiency);
-  };
+  // const passDataFromChild = (newValue) => {
+  //   setEnglishProficiency(newValue);
+  //   // console.log(englishProficiency);
+  // };
   //End of Data for End Point
 
   //useState for PopUpDialog
@@ -76,18 +54,18 @@ export const StudentCvUpdate = () => {
   //End of useState for PopUpDialog
 
   //Control Stack toggle
-  const [isStackOpen, setStackOpen] = useState({
-    englishProficiency: false,
-    programmingLanguages: false,
-    otherSkills: false,
-    projects: false,
-  });
+  // const [isStackOpen, setStackOpen] = useState({
+  //   englishProficiency: false,
+  //   programmingLanguages: false,
+  //   otherSkills: false,
+  //   projects: false,
+  // });
 
-  const toggleStack = (key) => {
-    setStackOpen((prevState) => {
-      prevState[key] = !prevState[key];
-    });
-  };
+  // const toggleStack = (key) => {
+  //   setStackOpen((prevState) => {
+  //     prevState[key] = !prevState[key];
+  //   });
+  // };
   //End of Control Stack toggle
 
   //Handling state for PopUpDialog
@@ -100,89 +78,6 @@ export const StudentCvUpdate = () => {
     });
   };
   //End of handling state for PopUpDialog
-
-  //useState for snackbar and dialog of pdf selector
-  const [open, setOpen] = React.useState(false);
-  const [errorOpen, setErrorOpen] = React.useState(false);
-  //end of useState for snackbar dialog of pdf selector
-
-  //handling dialog closing
-  const handleClose = () => {
-    setOpen(false);
-  };
-  //End of Handling dialog closing
-
-  //useStates for pdfviewer
-  const [PDFFile, setPDFFile] = useState(null);
-  const [viewPDF, setViewPDF] = useState(null);
-  //end of useStates for pdfviewer
-
-  //defining filetype for filepicker
-  const fileType = ["application/pdf"];
-  //end of defining filetype for filepicker
-
-  //handling the change in file picker
-  const handleChange = (e) => {
-    let selectedFile = e.target.files[0];
-    console.log(selectedFile.type);
-    if (selectedFile) {
-      if (selectedFile && fileType.includes(selectedFile.type)) {
-        let reader = new FileReader();
-        reader.readAsDataURL(selectedFile);
-        reader.onload = (e) => {
-          setPDFFile(selectedFile);
-          setViewPDF(e.target.result);
-        };
-      } else {
-        //if a selected file is not a pdf
-        setErrorOpen(true);
-        setPDFFile(null);
-      }
-    } else {
-      //if a file not selected
-      setErrorOpen(true);
-    }
-  };
-  //end of handling the changes in file picker
-
-  //view pdf
-  const viewButtonHandle = (e) => {
-    e.preventDefault();
-    if (viewPDF !== null) {
-      // setViewPDF(PDFFile);
-      // console.log(PDFFile);
-      setOpen(true);
-    } else {
-      setViewPDF(null);
-    }
-  };
-  //end of view pdf
-
-  //newpluging creation for pdf viewer
-  const newplugin = defaultLayoutPlugin();
-  //end of new plugin creation for pdf viewer
-
-  //End point
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log("EndPoint here");
-
-    const headers = {
-      "Content-Type": "multipart/form-data",
-      withCredentials: true,
-    };
-
-    const formData = new FormData();
-    formData.append("cv-file", PDFFile);
-
-    const res = await axios.post(
-      "http://localhost:5000/api/v1/undergraduate/info/upload-cv",
-      formData,
-      headers
-    );
-    console.log(res);
-  };
-  //end of the end point
 
   return (
     <Box sx={{ height: "88vh" }}>
@@ -232,7 +127,7 @@ export const StudentCvUpdate = () => {
                     }}
                     id={"EnglishProficiency"}
                   >
-                    <EnglishProficiency passDataFromChild={passDataFromChild} />
+                    <EnglishProficiency />
                   </PopUpDialog>
                 </Stack>
                 <Stack mt={1} direction={"row"} spacing={0.5}>
@@ -457,107 +352,6 @@ export const StudentCvUpdate = () => {
           </Grid>
           <Grid item xs={3}>
             <Stack direction="column" spacing={1} height={"100%"}>
-              {/* right bottom content here */}
-              <Box sx={{ height: "60%" }}>
-                <Tile sx={{ height: "100%" }}>
-                  <Stack
-                    height={"100%"}
-                    display={"flex"}
-                    justifyContent={"space-around"}
-                  >
-                    <Box>
-                      <Typography
-                        variant="body2"
-                        align="center"
-                        fontWeight={"bold"}
-                      >
-                        Upload your CV
-                      </Typography>
-                    </Box>
-                    <Box>
-                      <form onSubmit={handleSubmit}>
-                        <Typography variant="body2" mb={1}>
-                          Choose file:
-                        </Typography>
-                        <input type="file" onChange={handleChange} />
-                        <Stack direction={"row"} mt={1.5}>
-                          <Button
-                            variant="itms"
-                            size="itms-small"
-                            onClick={viewButtonHandle}
-                          >
-                            View
-                          </Button>
-                          <Button
-                            type="submit"
-                            variant="itms"
-                            size="itms-small"
-                          >
-                            Submit
-                          </Button>
-                        </Stack>
-                      </form>
-
-                      {/* viewing the cv dialog*/}
-                      <Dialog
-                        fullScreen
-                        open={open}
-                        onClose={handleClose}
-                        TransitionComponent={Transition}
-                      >
-                        <AppBar sx={{ position: "relative" }} elevation={0}>
-                          <Toolbar>
-                            <IconButton
-                              edge="start"
-                              color="#363853"
-                              onClick={handleClose}
-                              aria-label="close"
-                            >
-                              <CloseIcon />
-                            </IconButton>
-                            <Typography
-                              sx={{ ml: 2, flex: 1 }}
-                              variant="body2"
-                              fontWeight="bold"
-                            >
-                              Uploaded File
-                            </Typography>
-                          </Toolbar>
-                        </AppBar>
-                        <Box
-                          justifyContent="center"
-                          justifyItems="center"
-                          sx={{
-                            height: "900px",
-                            width: "100%",
-                          }}
-                          overflow-y="auto"
-                        >
-                          <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-                            {viewPDF && (
-                              <>
-                                <Viewer
-                                  fileUrl={viewPDF}
-                                  plugins={[newplugin]}
-                                />
-                              </>
-                            )}
-                            {!viewPDF && <>No PDF</>}
-                          </Worker>
-                        </Box>
-                      </Dialog>
-
-                      <StatusSnackBar
-                        trigger={errorOpen}
-                        setTrigger={setErrorOpen}
-                        severity="error"
-                        alertMessage="Error"
-                      />
-                    </Box>
-                  </Stack>
-                </Tile>
-              </Box>
-              {/* right top content here */}
               <Box sx={{ height: "100%" }}>
                 <Tile sx={{ height: "100%" }}>
                   <CVUpload />
