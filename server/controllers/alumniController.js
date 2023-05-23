554765ee668e245b5eee52c635b16d5c337ccf85
Nuwan Kirillawala <nuwan.sv.kirillawala@gmail.com>
2023-05-23 08:17:03 +0530

    CRUD for alumni added

--- server/controllers/alumniController.js
+++ server/controllers/alumniController.js
@@ -1,10 +1,12 @@
 const Alumni = require("../models/Alumni");
+const Company = require("../models/Company");
 const handleErrors = require("../utils/appErrors");
 const catchAsync = require('../utils/catchAsync');
 
-// Method = POST
-// Endpoint = "/create-alumni"
-// Description = create alumni-user
+// Method: POST
+// Endpoint: "/create"
+// Description: create alumni-user
+// User: admin
 module.exports.createAlumni = catchAsync(async (req, res) => {
     try {
         const { name, email, contactNo, regNo, graduatedYear, password } = req.body;
@@ -26,10 +28,53 @@ module.exports.createAlumni = catchAsync(async (req, res) => {
     }
 });
 
+// Method: GET
+// Endpoint: "/dashboard"
+// Description: get alumni dashboard
+// User: alumni
+module.exports.getAlumniDashboard = catchAsync(async (req, res) => {
+    try {
+        const userId = req.user.id;
+
+        const user = await Alumni.findById(userId);
+        const companyList = await Company.find();
+        
+        if (!user) {
+            return res.status(400).json({ error: "user not found" });
+        }
+
+        res.status(201).json({user, companyList});
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Method = GET
+// Endpoint = "/profile"
+// Description = get alumni profile
+// User: alumni
+module.exports.getAlumniProfile = catchAsync(async (req, res) => {
+    try {
+        const userId = req.user.id;
+
+        const user = await Alumni.findById(userId);
+        
+        if (!user) {
+            return res.status(400).json({ error: "user not found" });
+        }
+
+        res.status(201).json(user);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
 // Method = PATCH
-// Endpoint = "/update-alumni-profile"
+// Endpoint = "/profile"
 // Description = Update alumni profile
+// User: Alumni
 module.exports.updateAlumniProfile = catchAsync(async (req, res) => {
     try {
         const userId = req.user.id;
@@ -49,6 +94,75 @@ module.exports.updateAlumniProfile = catchAsync(async (req, res) => {
         }
 
         res.status(201).json(user);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Method = GET
+// Endpoint = "/user/:userId"
+// Description = get alumni
+// User: Admin
+module.exports.getAlumniUser = catchAsync(async (req, res) => {
+    try {
+        const userId = req.params.userId;
+
+        const user = await Alumni.findById(userId);
+        
+        if (!user) {
+            return res.status(400).json({ error: "user not found" });
+        }
+
+        res.status(201).json(user);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Method = PATCH
+// Endpoint = "/user/:userId"
+// Description = Update alumni
+// User: Admin
+module.exports.updateAlumniUser = catchAsync(async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        const { name, email, contactNo, regNo, graduatedYear } = req.body;
+
+        const update = { $set: { name, email, contactNo, regNo, graduatedYear } };
+        const options = { new: true };
+
+        const user = await Alumni.findByIdAndUpdate(
+            userId,
+            update,
+            options
+        );
+        
+        if (!user) {
+            return res.status(400).json({ error: "user not found" });
+        }
+
+        res.status(201).json(user);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Method = GET
+// Endpoint = "/user/all"
+// Description = get all alumni
+// User: Admin
+module.exports.getAllAlumniUsers = catchAsync(async (req, res) => {
+    try {
+        const alumniList = await Alumni.find();
+        
+        if (!alumniList) {
+            return res.status(400).json({ error: "any user not found" });
+        }
+
+        res.status(201).json(alumniList);
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
--- server/routes/alumniRoutes.js
+++ server/routes/alumniRoutes.js
@@ -1,12 +1,23 @@
-const { Router} = require('express')
+const { Router } = require('express')
 const alumniController = require('../controllers/alumniController');
 
 const router = Router();
 
-router.route('/create-alumni')
+router.route('/create')
     .post(alumniController.createAlumni)
 
-router.route('/update-alumni-profile')
+router.route('/dashboard')
+    .get(alumniController.getAlumniDashboard)
+
+router.route('/profile')
+    .get(alumniController.getAlumniProfile)
     .patch(alumniController.updateAlumniProfile)
+
+router.route('/user/:userId')
+    .get(alumniController.getAlumniUser)
+    .patch(alumniController.updateAlumniUser)
+
+router.route('/user/all')
+    .get(alumniController.getAllAlumniUsers)
 
 module.exports = router;
