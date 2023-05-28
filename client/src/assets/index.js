2b65bb857e4907dd33a828c12a30a4b4a84a7184
NoV4k <sanjanadiluka@gmail.com>
2023-05-28 22:01:00 +0530

    cvupload fixing

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -2,5 +2,6 @@ import Company from './Company.svg';
 import Student from './Student.svg';
 import Creative from './creative.png'
 import Report from './Report.svg'
+import UploadImg from './CVUpload.svg'
 
-export { Company, Student, Creative, Report };
+export { Company, Student, Creative, Report, UploadImg };
