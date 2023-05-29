d2664a6e807412a5538741dbffa0cfcb8b48a034
SSumu <sadeepalsumudu@gmail.com>
2023-05-29 09:23:41 +0530

    Structure was updated

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,288 +1,255 @@
-import * as React from "react";
-import { useState, useEffect } from "react";
-import {
-  Box,
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  IconButton,
-  ListItemButton,
-  ListItemIcon,
-  Stack,
-  Toolbar,
-} from "@mui/material";
-import CssBaseline from "@mui/material/CssBaseline";
-import Typography from "@mui/material/Typography";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import { Unilogo } from "../shared/Images/Unilogo";
-import { Grid } from "@mui/material";
-import {
-  ArrowBack,
-  Article,
-  Assessment,
-  Autorenew,
-  Badge,
-  Ballot,
-  Business,
-  ChevronLeft,
-  Dashboard,
-  Description,
-  Groups,
-  LocationCity,
-  Logout,
-  ManageAccounts,
-  Menu,
-  Notifications,
-  Settings,
-  Sort,
-  UploadFile,
-} from "@mui/icons-material";
-import { useNavigate } from "react-router-dom";
-import styled from "@emotion/styled";
-import MuiDrawer from "@mui/material/Drawer";
-import useAuth from "../../Hooks/useAuth";
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, ListItemButton, ListItemIcon, Stack, Toolbar } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import { Unilogo } from '../shared/Images/Unilogo';
+import { Grid } from '@mui/material';
+import { ArrowBack, Article, Assessment, Autorenew, Badge, Ballot, Business, ChevronLeft, Dashboard, Description, Groups, LocationCity, Logout, ManageAccounts, Menu, Notifications, Settings, Sort, UploadFile } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
+import styled from '@emotion/styled';
+import MuiDrawer from '@mui/material/Drawer';
+import useAuth from '../../Hooks/useAuth';
 
 const drawerWidth = 240;
 
 const openedMixin = (theme) => ({
   width: drawerWidth,
-  transition: theme.transitions.create("width", {
+  transition: theme.transitions.create('width', {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.enteringScreen,
   }),
-  overflowX: "hidden",
+  overflowX: 'hidden',
 });
 
 const closedMixin = (theme) => ({
-  transition: theme.transitions.create("width", {
+  transition: theme.transitions.create('width', {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  overflowX: "hidden",
+  overflowX: 'hidden',
   width: `calc(${theme.spacing(6)} + 1px)`,
-  display: "flex",
-  alignItems: "center",
-  [theme.breakpoints.up("sm")]: {
+  display: 'flex',
+  alignItems: 'center',
+  [theme.breakpoints.up('sm')]: {
     width: `calc(${theme.spacing(9)} + 1px)`,
   },
 });
 
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  flexShrink: 0,
-  whiteSpace: "nowrap",
-  boxSizing: "border-box",
-  ...(open && {
-    ...openedMixin(theme),
-    "& .MuiDrawer-paper": openedMixin(theme),
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
   }),
-  ...(!open && {
-    ...closedMixin(theme),
-    "& .MuiDrawer-paper": closedMixin(theme),
-  }),
-}));
+);
 
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
   padding: theme.spacing(0, 1),
   ...theme.mixins.toolbar,
-  justifyContent: "flex-end",
+  justifyContent: 'flex-end',
 }));
 
 const buttonStyles = {
   height: 40,
-  "&:hover": {
-    bgcolor: "#f4f6fc",
-    color: "black",
-  },
+  '&:hover': {
+    bgcolor: '#f4f6fc',
+    color: 'black'
+  }
 };
 
 const users = [
   {
-    name: "Student/Undergraduate",
+    name: 'Student/Undergraduate',
     items: [
       {
         id: 1,
-        primaryText: "Dashboard",
+        primaryText: 'Dashboard',
         icon: <Dashboard />,
-        element: "/student-dashboard",
+        element: '/student-dashboard'
       },
       {
         id: 2,
-        primaryText: "Intern Application",
+        primaryText: 'Intern Application',
         icon: <Article />,
-        element: "/intern-application/menu",
+        element: '/intern-application/menu'
       },
       {
         id: 3,
-        primaryText: "Intern Status",
+        primaryText: 'Intern Status',
         icon: <Assessment />,
-        element: "/student-internstatus",
+        element: '/student-company'
       },
       {
         id: 4,
-        primaryText: "Internship",
+        primaryText: 'Internship',
         icon: <Badge />,
-        element: "/student-internship",
+        element: '/student-company'
       },
       {
         id: 5,
-        primaryText: "Company",
+        primaryText: 'Company',
         icon: <Business />,
-        element: "/company/all",
+        element: '/company/all'
       },
       {
         id: 6,
-        primaryText: "Report Submision",
+        primaryText: 'Report Submision',
         icon: <UploadFile />,
-        element: "/student-report",
+        element: '/student-report'
       },
       {
         id: 7,
-        primaryText: "Private Notes",
+        primaryText: 'Private Notes',
         icon: <Description />,
-        element: "/private-note",
+        element: '/notice'
       },
       {
         id: 8,
-        primaryText: "Notices",
+        primaryText: 'Notices',
         icon: <Notifications />,
-        element: "/notice",
-      },
-    ],
+        element: '/notice'
+      }
+    ]
   },
   {
-    name: "Admin",
+    name: 'Admin',
     items: [
       {
         id: 1,
-        primaryText: "Dashboard",
+        primaryText: 'Dashboard',
         icon: <Dashboard />,
-        element: "admin-dashboard",
+        element: 'admin-dashboard'
       },
       {
         id: 2,
-        primaryText: "User Management",
+        primaryText: 'User Management',
         icon: <ManageAccounts />,
-        element: "/manage-user",
+        element: '/manage-user'
       },
       {
         id: 3,
-        primaryText: "Companies",
+        primaryText: 'Companies',
         icon: <LocationCity />,
-        element: "/manage-company",
+        element: '/manage-company'
       },
       {
         id: 4,
-        primaryText: "Results",
+        primaryText: 'Results',
         icon: <Sort />,
-        element: "/result-sheet/view",
+        element: '/result-sheet/view'
       },
       {
         id: 5,
-        primaryText: "Intern Process",
+        primaryText: 'Intern Process',
         icon: <Autorenew />,
-        element: "/intern-process/type",
+        element: '/intern-process/type'
       },
       {
         id: 6,
-        primaryText: "Intern Reports",
+        primaryText: 'Intern Reports',
         icon: <Ballot />,
-        element: "/report-portal",
+        element: '/report-portal'
       },
       {
         id: 7,
-        primaryText: "Private Notes",
+        primaryText: 'Private Notes',
         icon: <Description />,
-        element: "/deletenotices",
+        element: '/deletenotices'
       },
       {
         id: 8,
-        primaryText: "Notices",
+        primaryText: 'Notices',
         icon: <Notifications />,
-        element: "/notice",
-      },
-    ],
+        element: '/deletenotices'
+      }
+    ]
   },
   {
-    name: "Supervisor",
+    name: 'Supervisor',
     items: [
       {
         id: 1,
-        primaryText: "Dashboard",
+        primaryText: 'Dashboard',
         icon: <Dashboard />,
-        element: "/student-dashboard",
+        element: '/student-dashboard'
       },
       {
         id: 2,
-        primaryText: "Intern Members",
+        primaryText: 'Intern Members',
         icon: <Groups />,
-        element: "/intern-process-student",
+        element: '/intern-process-student'
       },
       {
         id: 3,
-        primaryText: "Report Submission",
+        primaryText: 'Report Submission',
         icon: <UploadFile />,
-        element: "/supervisor-report-portal",
+        element: '/student-company'
       },
       {
         id: 4,
-        primaryText: "Private Notes",
+        primaryText: 'Private Notes',
         icon: <Description />,
-        element: "/daily-report-list",
-      },
-    ],
+        element: '/daily-report-list'
+      }
+    ]
   },
   {
-    name: "Alumni",
+    name: 'Alumni',
     items: [
       {
         id: 1,
-        primaryText: "Dashboard",
+        primaryText: 'Dashboard',
         icon: <Dashboard />,
-        element: "/student-dashboard",
+        element: '/student-dashboard'
       },
       {
         id: 2,
-        primaryText: "Company Ratings",
+        primaryText: 'Company Ratings',
         icon: <Ballot />,
-        element: "/intern-process-student",
+        element: '/intern-process-student'
       },
       {
         id: 3,
-        primaryText: "Update Workline",
+        primaryText: 'Update Workline',
         icon: <LocationCity />,
-        element: "/student-company",
+        element: '/student-company'
       },
-    ],
-  },
+    ]
+  }
 ];
 
 const controlItems = [
   {
     id: 1,
-    label: "Settings",
+    label: 'Settings',
     icon: <Settings />,
-    page: "/student-settings",
+    page: "/student-settings"
   },
   {
     id: 2,
-    label: "Back",
+    label: 'Back',
     icon: <ArrowBack />,
-    page: "back",
+    page: 'back'
   },
   {
     id: 3,
-    label: "Log out",
+    label: 'Log out',
     icon: <Logout />,
-    page: "/logout",
-  },
+    page: '/logout'
+  }
 ];
 
 export default function Sidebar() {
@@ -290,8 +257,8 @@ export default function Sidebar() {
   const { logout, user, isAuthenticated } = useAuth();
 
   const handleControlItem = (page) => {
-    if (page === "back") {
-      window.history.back();
+    if (page === 'back') {
+      window.history.back()
     } else {
       navigate(page);
     }
@@ -304,22 +271,23 @@ export default function Sidebar() {
 
   const [currentUser, setCurrentUser] = useState();
 
-  useEffect(() => {
-    if (user && user.role) {
-      if (user.role === "undergraduate") {
-        setCurrentUser(users[0]);
-      } else if (
-        user.role === "department-coordinator" ||
-        user.role === "system-admin"
-      ) {
+  useEffect(() => {  
+    if(user && user.role){
+      if(user.role === 'undergraduate'){
+        setCurrentUser(users[0])
+      }
+      else if(user.role === 'department-coordinator' || user.role === 'system-admin'){
         setCurrentUser(users[1]);
-      } else if (user.role === "supervisor") {
+      }
+      else if(user.role === 'supervisor'){
         setCurrentUser(users[2]);
-      } else if (user.role === "alumni") {
+      }
+      else if(user.role === 'alumni'){
         setCurrentUser(users[3]);
       }
-    }
+    }  
   }, [user]);
+
 
   const handleCurrentUserItem = (user, element) => {
     setCurrentUser(user);
@@ -335,7 +303,7 @@ export default function Sidebar() {
   const handleLogoutSubmit = async (e) => {
     e.preventDefault();
     await logout();
-    navigate("/login");
+    navigate('/login');
   };
 
   const handleClose = () => {
@@ -343,7 +311,7 @@ export default function Sidebar() {
   };
 
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <Dialog
         open={openLogoutDialogBox}
@@ -351,7 +319,9 @@ export default function Sidebar() {
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">{"Logout?"}</DialogTitle>
+        <DialogTitle id="alert-dialog-title">
+          {"Logout?"}
+        </DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
             Are you sure you want to log out?
@@ -366,14 +336,14 @@ export default function Sidebar() {
       </Dialog>
       <Drawer
         sx={{
-          textAlign: "center",
+          textAlign: 'center',
           flexShrink: 0,
-          "& .MuiDrawer-paper": {
-            boxSizing: "border-box",
-            bgcolor: "#4665d2",
-            color: "#f4f6fc",
+          '& .MuiDrawer-paper': {
+            boxSizing: 'border-box',
+            bgcolor: '#4665d2',
+            color: '#f4f6fc',
           },
-          overflowX: "hidden",
+          overflowX: 'hidden'
         }}
         variant="permanent"
         anchor="left"
@@ -381,20 +351,17 @@ export default function Sidebar() {
       >
         <Box>
           {open ? (
-            <Box
-              display={"flex"}
-              justifyContent={open ? "flex-end" : "space-between"}
-            >
+            <Box display={'flex'} justifyContent={open ? 'flex-end' : 'space-between'}>
               <DrawerHeader>
-                <IconButton onClick={toggleDrawer} sx={{ color: "inherit" }}>
+                <IconButton onClick={toggleDrawer} sx={{ color: 'inherit' }}>
                   <ChevronLeft />
                 </IconButton>
               </DrawerHeader>
             </Box>
           ) : (
-            <Box display={"flex"} justifyContent={"center"}>
+            <Box display={'flex'} justifyContent={'center'}>
               <DrawerHeader>
-                <IconButton onClick={toggleDrawer} sx={{ color: "inherit" }}>
+                <IconButton onClick={toggleDrawer} sx={{ color: 'inherit' }}>
                   <Menu />
                 </IconButton>
               </DrawerHeader>
@@ -404,25 +371,24 @@ export default function Sidebar() {
         <Stack>
           <Toolbar
             sx={{
-              display: "flex",
-              justifyContent: "center",
+              display: 'flex',
+              justifyContent: 'center'
             }}
           >
             {open && (
-              <Stack position={"relative"} top={0}>
+              <Stack position={'relative'} top={0}>
                 <Grid container justifyContent="center">
-                  <Unilogo width="50px" height="100px" />
+                  <Unilogo width='50px' height='100px' />
                 </Grid>
                 <Typography
-                  variant={"h6"}
-                  fontWeight={"bold"}
+                  variant={'h6'}
+                  fontWeight={'bold'}
                   letterSpacing={5}
                   sx={{
-                    position: "relative",
+                    position: 'relative',
                     top: 5,
                     lineHeight: 1.2,
-                  }}
-                >
+                  }}>
                   ITMS
                 </Typography>
               </Stack>
@@ -431,76 +397,74 @@ export default function Sidebar() {
         </Stack>
         <Stack
           sx={{
-            position: "absolute",
+            position: 'absolute',
             top: 200,
             left: 0,
-            right: 0,
+            right: 0
           }}
         >
           {users.map((user) => (
-            <ListItem key={user.name} disablePadding></ListItem>
+            <ListItem key={user.name} disablePadding>
+            </ListItem>
           ))}
           <Stack
             sx={{
-              display: "flex",
-              flexDirection: "column",
+              display: 'flex',
+              flexDirection: 'column'
             }}
           >
-            {currentUser &&
-              currentUser.items.map((item) => (
-                <ListItemButton
-                  key={item.id}
-                  sx={buttonStyles}
-                  onClick={() =>
-                    handleCurrentUserItem(currentUser, item.element)
-                  }
+            {currentUser && currentUser.items.map((item) => (
+              <ListItemButton
+                key={item.id}
+                sx={buttonStyles}
+                onClick={() => handleCurrentUserItem(currentUser, item.element)}
+              >
+                <ListItemIcon
+                  sx={{
+                    color: 'inherit',
+                    justifyContent: 'center',
+                    position: 'relative',
+                    left: open ? '0' : '-8px',
+                    transition: 'left 0.3s'
+                  }}
                 >
-                  <ListItemIcon
-                    sx={{
-                      color: "inherit",
-                      justifyContent: "center",
-                      position: "relative",
-                      left: open ? "0" : "-8px",
-                      transition: "left 0.3s",
-                    }}
-                  >
-                    {item.icon}
-                  </ListItemIcon>
-                  {open && <ListItemText primary={item.primaryText} />}
-                </ListItemButton>
-              ))}
-          </Stack>
+                  {item.icon}
+                </ListItemIcon>
+                {open && (
+                  <ListItemText primary={item.primaryText} />
+                )}
+              </ListItemButton>
+            ))}
+          </Stack>  
         </Stack>
         <Stack
           sx={{
-            position: "absolute",
+            position: 'absolute',
             bottom: 20,
             left: 0,
-            right: 0,
+            right: 0
           }}
         >
           {controlItems.map((controlItem) => (
             <ListItemButton
               key={controlItem.id}
               sx={buttonStyles}
-              onClick={(e) =>
-                controlItem.id === 3
-                  ? handleLogout(e)
-                  : handleControlItem(controlItem.page)
-              }
+              onClick={(e) => controlItem.id === 3 ? handleLogout(e) : handleControlItem(controlItem.page)}
             >
               <ListItemIcon
                 sx={{
-                  color: "inherit",
-                  justifyContent: "center",
-                  position: "relative",
-                  left: open ? "0" : "-8px",
-                  transition: "left 0.3s",
+                  color: 'inherit',
+                  justifyContent: 'center',
+                  position: 'relative',
+                  left: open ? '0' : '-8px',
+                  transition: 'left 0.3s'
                 }}
               >
                 {controlItem.icon}
               </ListItemIcon>
-              {open && <ListItemText primary={controlItem.label} />}
+              {open && (
+                <ListItemText primary={controlItem.label} />
+              )}
             </ListItemButton>
           ))}
         </Stack>
